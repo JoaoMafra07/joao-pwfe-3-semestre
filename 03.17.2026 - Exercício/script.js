9f766bdecb4e8050eb371e34e93bc1cf94f8e54c
JoaoMafra07 <jrlorimafra@gmail.com>
2026-03-17 11:25:24 -0300

    Iniciando a atividade

---
+++ 03.17.2026 - Exercício/script.js
@@ -0,0 +1,27 @@
+// function madTalk()
+// {
+//     const verbo = document.getElementById("verbo").value;
+//     const nome = document.getElementById("nome").value;
+//     const adjetivo = document.getElementById("adjetivo").value;
+
+//     const frase = document.getElementById("frase")
+
+//     frase.innerText = `${nome} é ${verbo} pra fazer ${adjetivo}`
+// }
+
+// const botao = document.getElementById("botao")
+
+// botao.addEventListener('click', madTalk)
+
+// FUNÇÕES QUE PODEM AJUDAR A FAZER A ATIVIDADE
+// 1. toString
+// 2. length
+// 3. slice
+// 4. split
+// 5. join
+// 6. push / pop
+// 7. shift / unshift
+// 8. replace
+
+// SE EU QUISER ME AVENTURAR: REGEX
+
